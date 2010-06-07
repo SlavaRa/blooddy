9c216e7ff90c789361d32d32dab33c1485420422
mryzhy <mryzhy@5f840103-6b48-0410-a467-6bbae6820df1>
2010-06-07 01:23:25 +0000

    base64 decode crc32

--- trunk/blooddy_crypto/src/by/blooddy/crypto/Base64.hx
+++ trunk/blooddy_crypto/src/by/blooddy/crypto/Base64.hx
@@ -1,5 +1,6 @@
 package by.blooddy.crypto;
 
+import flash.Error;
 import flash.Memory;
 import flash.utils.ByteArray;
 
@@ -27,12 +28,13 @@ private class TMP {
 	public static inline function encode(bytes:ByteArray, insertNewLines:Bool=false):String {
 
 		var len:UInt = bytes.length;
+		var pos:UInt = bytes.position;
 
 		var rest:UInt = len % 3;
 		var bytesLength:UInt = len - 3;
 
 		bytes.position = len;
-		bytes.writeMultiByte( 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/', 'us-ascii' );
+		bytes.writeUTFBytes( 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/' );
 
 		var resultLength:UInt = Std.int( len / 3 ) * 4 + ( rest > 0 ? 4 : 0 );
 		bytes.length += resultLength + ( insertNewLines ? Std.int( resultLength / 76 ) : 0 );
@@ -91,13 +93,107 @@ private class TMP {
 		var result:String = bytes.readUTFBytes( bytes.bytesAvailable );
 
 		bytes.length = len;
+		bytes.position = pos;
 		
 		return result;
 
 	}
 
 	public static inline function decode(str:String):ByteArray {
-		return null;
+
+		var len:UInt = Std.int( str.length * 0.75 );
+
+		var bytes:ByteArray = new ByteArray();
+		bytes.position = len;
+		bytes.writeUTFBytes( '\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x40\x3e\x40\x40\x40\x3f\x34\x35\x36\x37\x38\x39\x3a\x3b\x3c\x3d\x40\x40\x40\x40\x40\x40\x40\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x40\x40\x40\x40\x40\x40\x1a\x1b\x1c\x1d\x1e\x1f\x20\x21\x22\x23\x24\x25\x26\x27\x28\x29\x2a\x2b\x2c\x2d\x2e\x2f\x30\x31\x32\x33\x40\x40\x40\x40\x40' );
+		bytes.writeUTFBytes( str );
+
+		var bytesLength:UInt = bytes.length - 4;
+
+		if ( bytes.length < 1024 ) bytes.length = 1024;
+		Memory.select( bytes );
+
+		var i:UInt = len + 128;
+		var j:UInt = 0;
+
+		var insertNewLines:Bool = Memory.getByte( i + 76 ) == 10;
+
+		var a:Int;
+		var b:Int;
+		var c:Int;
+		var d:Int;
+
+		while ( i < bytesLength ) {
+
+			a = getByte( i++, len );
+			b = getByte( i++, len );
+			c = getByte( i++, len );
+			d = getByte( i++, len );
+
+			Memory.setByte( j++, ( a << 2 ) | ( b >> 4 ) );
+			Memory.setByte( j++, ( b << 4 ) | ( c >> 2 ) );
+			Memory.setByte( j++, ( c << 6 ) |   d        );
+			
+			if ( insertNewLines && j % 57 == 0 && Memory.getByte( i++ ) != 10 ) {
+				throwError();
+			}
+
+		}
+
+		if ( i != bytesLength ) throwError();
+
+		a = getByte( i++, len );
+		b = getByte( i++, len );
+		Memory.setByte( j++, ( a << 2 ) | ( b >> 4 ) );
+
+		c = getByte2( i++, len );
+		if ( c != -1 ) {
+			Memory.setByte( j++, ( b << 4 ) | ( c >> 2 ) );
+			d = getByte2( i++, len );
+			if ( d != -1 ) {
+				Memory.setByte( j++, ( c << 6 ) | d );
+			}
+		} else {
+			if ( getByte2( i++, len ) != -1 ) throwError();
+		}
+
+		bytes.length = j;
+		bytes.position = 0;
+		
+		return bytes;
+	}
+
+	/**
+	 * @private
+	 */
+	private static inline function getByte(index:UInt, offset:UInt):Int {
+		var v:Int = Memory.getByte( index );
+		if ( v & 0x80 != 0 ) throwError();
+		v = Memory.getByte( offset + v );
+		if ( v == 0x40 ) throwError();
+		return v;
+	}
+
+	/**
+	 * @private
+	 */
+	private static inline function getByte2(index:UInt, offset:UInt):Int {
+		var v:Int = Memory.getByte( index );
+		if ( v & 0x80 != 0 ) throwError();
+		if ( v != 61 ) {
+			v = Memory.getByte( offset + v );
+			if ( v == 0x40 ) throwError();
+		} else {
+			v = -1;
+		}
+		return v;
+	}
+
+	/**
+	 * @private
+	 */
+	private static inline function throwError():Void {
+		Error.throwError( Error, 0 );
 	}
 
 }
